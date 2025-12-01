22324dc21db40e778484a4a14bdefca2c6387a24
João Neto <se.joao.neto@gmail.com>
2025-12-01 22:43:33 +0000

    passing first test

--- src/Day01/main.py
+++ src/Day01/main.py
@@ -14,8 +14,29 @@ def main():
         print(f"No input found.")
         return
 
+    num_pos = 100
+    current_pos = 50
+    num_zeros = 0
+
     for line in data:
-        print(line)
+        direction = line[0]
+        ammount = int(line[1:])
+
+        if direction == "R":
+            current_pos += ammount
+        if direction == "L":
+            current_pos -= ammount
+
+        current_pos %= num_pos
+
+        if current_pos == 0:
+            num_zeros += 1
+
+        print(
+            f"Direction: {direction}. Ammount: {ammount}. Current position: {current_pos}"
+        )
+
+    print(f"Number of times at 0: {num_zeros}")
 
 
 if __name__ == "__main__":
